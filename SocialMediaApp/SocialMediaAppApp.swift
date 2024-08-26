d6b43d311e863aa90e2c401f1e7f3120e8252df4
Abhinav Nagar <abhinavnagar26966@gmail.com>
2024-08-26 07:54:52 +0530

    Initial Commit

---
+++ SocialMediaApp/SocialMediaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaAppApp.swift
+//  SocialMediaApp
+//
+//  Created by Abhinav Nagar on 26/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
